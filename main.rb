a29557db9b988e25e96a1d0e6055d22de01cb0ec
Joe Roberts <joe@zefer.co.uk>
2020-05-30 12:58:34 +0100

    Write the energy usage to InfluxDB

--- main.rb
+++ main.rb
@@ -1,9 +1,24 @@
+require 'influxdb'
 require_relative 'lib/client'
 require_relative 'lib/electricity_usage'
 
 client = Client.new
 
+influxdb = InfluxDB::Client.new(
+  url: ENV['INFLUXDB_URL'] || 'http://user:pass@localhost:8086/dbname?retry=3'
+)
+
 loop do
   message = client.receive
-  puts ElectricityUsage.from_raw(message).to_s
+  usage = ElectricityUsage.from_raw(message)
+  next unless usage
+
+  data = {
+    values: {
+      current: usage.current,
+      today: usage.today
+    }
+  }
+
+  influxdb.write_point('lw_electricity', data)
 end
