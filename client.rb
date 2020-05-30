21b45b9d413326dfbfd7ba4da108d76548f98880
Joe Roberts <joe@zefer.co.uk>
2020-05-30 10:10:08 +0100

    A basic working UDP read/print of energy usage

---
+++ client.rb
@@ -0,0 +1,31 @@
+require 'socket'
+require 'ipaddr'
+require 'json'
+
+class Client
+  MULTICAST_ADDR = '224.0.0.1'.freeze
+  BIND_ADDR = '0.0.0.0'.freeze
+  PORT = 9761
+
+  def initialize
+    @socket = UDPSocket.new
+    membership = IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new(BIND_ADDR).hton
+
+    @socket.setsockopt(:IPPROTO_IP, :IP_ADD_MEMBERSHIP, membership)
+    @socket.setsockopt(:IPPROTO_IP, :IP_MULTICAST_TTL, 1)
+    @socket.setsockopt(:SOL_SOCKET, :SO_REUSEPORT, 1)
+
+    @socket.bind(BIND_ADDR, PORT)
+  end
+
+  def listen
+    loop do
+      m, = @socket.recvfrom(2000)
+      m = m.sub('*!', '')
+      record = JSON.parse(m)
+      p record
+    end
+  end
+end
+
+Client.new.listen
